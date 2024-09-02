734fcc03554b7c43e28845e31a41c64edc8ae6fb
Teklaw7 <timothee.teyssier@cpe.fr>
2024-09-02 12:14:44 +0200

    [feat] blank page with some buttons to increment a counter

--- mole-checking/src/App.vue
+++ mole-checking/src/App.vue
@@ -1,47 +1,27 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Count : {{count}}</h1>
+  <!-- <button @click="count++">Increment</button> -->
+  <!-- <button @click="count--">Decrement</button> -->
+  <button v-on:click="increment">Increment</button>
+  <button v-on:click="decrement">Decrement</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import { ref } from 'vue'
+const count = ref(10)
+
+const increment = () => {
+  count.value++
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+const decrement = () => {
+  count.value--
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
+<style>
+  h1 {
+    color: blue;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
