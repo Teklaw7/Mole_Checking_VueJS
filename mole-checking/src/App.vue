a46fbe05193c8093100fcc78a07c0afd722e8363
Teklaw7 <timothee.teyssier@cpe.fr>
2024-09-02 12:22:00 +0200

    [feat] use of v-conditions

--- mole-checking/src/App.vue
+++ mole-checking/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>Count : {{count}}</h1>
+  <!-- <div v-show="count >= 10">Count is greater than or equal to 10</div> -->
+  <div v-if="count >= 5">Count is greater than or equal to 5</div>
+  <div v-else>Count is less than 5</div>
   <!-- <button @click="count++">Increment</button> -->
   <!-- <button @click="count--">Decrement</button> -->
   <button v-on:click="increment">Increment</button>
@@ -8,7 +11,7 @@
 
 <script setup>
 import { ref } from 'vue'
-const count = ref(10)
+const count = ref(0)
 
 const increment = () => {
   count.value++
