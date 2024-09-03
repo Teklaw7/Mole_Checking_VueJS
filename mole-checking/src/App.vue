e0591347c44536762afbb76da274dd57ced96810
Teklaw7 <timothee.teyssier@cpe.fr>
2024-09-03 17:51:47 +0200

    [feat] home page img only on home page

--- mole-checking/src/App.vue
+++ mole-checking/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <Header></Header>
-  <img src="../pictures/mole-checking.jpg" alt="Mole Checking" />
   <RouterView></RouterView>
 </template>
 
@@ -8,8 +7,3 @@
   import Header from './components/Header/Header.vue';
 </script>
 
-<style>
-  img {
-    width: 100%;
-  }
-</style> 
--- mole-checking/src/views/Home.vue
+++ mole-checking/src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
+  <img src="../../pictures/mole-checking.jpg" alt="Mole Checking" />
 </template>
 
 <script setup>
 </script>
+
+<style>
+  img {
+    width: 100%;
+  }
+</style> 
